91f34d58fec80812cbc6603531010fc49bc48a15
Richard Kwok <richardjkwok@gmail.com>
2016-09-17 08:43:19 -0400

    Separated my face

--- face.py
+++ face.py
@@ -1,3 +1,5 @@
+import re
+
 import pyglet
 import facepp
 import apiclient
@@ -15,6 +17,26 @@ DEV_KEY = 'AIzaSyCy4FtXwyNm6fx6j84AUI_GM9ZYrcrEGYk '
 # Source image
 IMAGE_PATH = "C:/Users/Richard/Desktop/me.jpg"
 
+def getEyes(landmarks):
+
+	return [(landmarks[key]['x'], landmarks[key]['y']) for key in landmarks if re.search("_eye_", key)]
+
+def getEyebrows(landmarks):
+
+	return [(landmarks[key]['x'], landmarks[key]['y']) for key in landmarks if re.search("eyebrow", key)]
+
+def getNose(landmarks):
+
+	return [(landmarks[key]['x'], landmarks[key]['y']) for key in landmarks if re.search("nose", key)]
+
+def getMouth(landmarks):
+
+	return [(landmarks[key]['x'], landmarks[key]['y']) for key in landmarks if re.search("mouth", key)]
+
+def getShape(landmarks):
+
+	return [(landmarks[key]['x'], landmarks[key]['y']) for key in landmarks if re.match("contour", key)]
+
 #############################################################
 # Main execution
 #############################################################
@@ -26,5 +48,16 @@ prediction_service.hostedmodels()
 facepp_api = facepp.API(API_KEY, API_SECRET, srv=API_SERVER)
 
 face = facepp_api.detection.detect(img=facepp.File(IMAGE_PATH))
+landmark = facepp_api.detection.landmark(face_id=face['face'][0]['face_id'], type='83p')
 
-print("{0} {1}".format(face['face'][0]['attribute']['race']['value'], face['face'][0]['attribute']['gender']['value']))
+window = pyglet.window.Window()
+
+vertex_list = pyglet.graphics.vertex_list(len(getEyes(landmark["result"][0]["landmark"])), ('v2f', (value for point in getEyes(landmark["result"][0]["landmark"]) for value in point)))
+
+@window.event
+def on_draw():
+    window.clear()
+    vertex_list.draw(pyglet.gl.GL_POINTS)
+
+pyglet.app.run()
+
